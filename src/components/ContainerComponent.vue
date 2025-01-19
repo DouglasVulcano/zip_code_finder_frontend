4813caf76befa9e66f8493216bd31be22ac694e0
DouglasVulcano <vulcanodouglas1708@gmail.com>
2025-01-19 18:01:36 -0300

    first commit

---
+++ src/components/ContainerComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  padding: 20px;
+  border-radius: 10px;
+  background-color: #fff;
+}
+</style>
---
+++ src/views/SearchView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import ContainerComponent from '../components/ContainerComponent.vue'
+</script>
+
+<template>
+  <main>
+    <ContainerComponent>
+      <h1>This is an home page</h1>
+    </ContainerComponent>
+  </main>
+</template>
